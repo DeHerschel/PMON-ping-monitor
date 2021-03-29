17add898441d3709b9452b0460fed9be25e8fa0f
DeHerschel <e.deherschel@gmail.com>
2021-03-29 22:26:08 +0200

    ref

--- pmon_functions.sh
+++ pmon_functions.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-
-#######################
-#   GLOBAL VARIABLES  #
-#######################
-
 source messages.sh
-declare -A ips; #Asociative array ips[mac-->IP, mac-->IP,...]
+unset HOST
 LOG="event.log";
 DATE="$(date)";
 
@@ -15,103 +10,69 @@ DATE="$(date)";
 
 function validMac() { #valid mac?
 	echo "$1" | egrep "^^([a-fA-F0-9]{2}:){5}([a-fA-F0-9]{2})$"  > /dev/null
-	if [ "$?" == 1 ]; then
-			return 1;
-	fi
+	[ "$?" == 0 ] || return 1;
 }
 function macHaveIp() {
-	mac="$1";
-	ip=$(echo "$scan" | grep "$mac" | awk '{print $1}');
-	ip_comp=$(echo "$scan" | grep "$mac" | awk '{print $1}' | wc -l);
+	local ip=$(echo "$scan" | grep "$1" | awk '{print $1}');
+	local ip_comp=$(echo "$scan" | grep "$1" | awk '{print $1}' | wc -l);
 	if  [ $ip_comp == 0 ]; then #Not in arp table
 		ipNotFoundMsg;
 		return 2;
 	fi
-	ips[$mac]="$ip";
+	echo $ip;
 }
-function pingIp() {
-	macHaveIp $1 || return;
-	if [ "$ifc" ]; then #selected iface
-		aae=0; #alive after errors
-		ping -O "${ips[$1]}" -I "$ifc" | while read -r line; do #ping and read lines
-			#no answer ---> eroor mode ---> alert & log
-			echo -e "\n${ips[$1]}: ";
-			if [[ "$line" =~ "Unreachable" ]] || [[ "$line" =~ "no answer" ]]; then
-				if [[ "$errormode" == 1 ]]; then
-					echo -e " \e[101;1;97m WARNING: $line\e[m";
-				else
-					echo -e "\e[101;1;97m$line\e[m" && echo -e "${DATE}: ${line}" >> "$LOG";
-					echo -e "\n\e[101;1;97m${hostdown_msg}\e[m\n" && echo -e "${DATE}: ${hostdown_msg}" >> "$LOG";
-					errormode=1;
-				fi
-			else #good answer
-				#error mode ---> alert & log
-				if [[ $errormode == 1 ]]; then
-					let aae=aae+1; #count aae
-					if [ "$aae" == 30 ]; then #30 good ping after error ends error mode
-						echo -e "\e[42;1;97m${line}\e[m" && echo -e "${DATE}: ${line}" >> "$LOG";
-						echo -e "\n\e[42;1;97m${hoststable_msg}\e[m\n" && echo -e "${DATE}: ${hoststable_msg}" >> "$LOG";
-						aae=0; #reset AAE
-						errormode=0;
-					elif [ "$aae" == 1 ]; then #first good ping after error
-						echo -e "\e[42;1;97m${line}\e[m" && echo -e "${DATE}: ${line}" >> "$LOG";
-						echo -e "\n\e[42;1;97m${DATE} HOST IS UP\e[m\n" && echo -e "${DATE} HOST IS UP" >> "$LOG"
-					else
-						echo -e "\e[42;1;97m${line}\e[m";
-					fi
-				else #no error
-					echo "$line";
-				fi
-			fi
-		done &
-		pingpids=(${pingpids[@]} $!)
-	else #default iface
-		aae=0; #alive after errors
-		ping -O "${ips[$1]}" | while read -r line; do #ping and read lines
-			#no answer ---> eroor mode ---> alert & log
-			echo -e "\n${ips[$1]}: ";
-			if [[ "$line" =~ "Unreachable" ]] || [[ "$line" =~ "no answer" ]]; then
-				if [[ "$errormode" == 1 ]]; then
-					echo -e " \e[101;1;97m WARNING: $line\e[m";
-				else
-					echo -e "\e[101;1;97m$line\e[m" && echo -e "${DATE}: ${line}" >> "$LOG";
-					echo -e "\n\e[101;1;97m${hostdown_msg}\e[m\n" && echo -e "${DATE}: ${hostdown_msg}" >> "$LOG";
-					errormode=1;
-				fi
-			else #good answer
-				#error mode ---> alert & log
-				if [[ $errormode == 1 ]]; then
-					let aae=aae+1; #count aae
-					if [ "$aae" == 30 ]; then #30 good ping after error ends error mode
-						echo -e "\e[42;1;97m${line}\e[m" && echo -e "${DATE}: ${line}" >> "$LOG";
-						echo -e "\n\e[42;1;97m${hoststable_msg}\e[m\n" && echo -e "${DATE}: ${hoststable_msg}" >> "$LOG";
-						aae=0; #reset AAE
-						errormode=0;
-					elif [ "$aae" == 1 ]; then #first good ping after error
-						echo -e "\e[42;1;97m${line}\e[m" && echo -e "${DATE}: ${line}" >> "$LOG";
-						echo -e "\n\e[42;1;97m${DATE} HOST IS UP\e[m\n" && echo -e "${DATE} HOST IS UP" >> "$LOG"
-					else
-						echo -e "\e[42;1;97m${line}\e[m";
-					fi
-				else #no error
-					echo "$line";
-				fi
-			fi
-		done &
-		pingpids=(${pingpids[@]} $!)
+function pingHost() { #ARGS: "mac"/"host" MAC/HOST IFC
+	local aae=0; #alive after errors
+	local errormode=0;
+	[[ $IFC ]] && local ifc="-I $IFC"; 
+	if [[ "$1" == "mac" ]]; then
+		HOST=$(macHaveIp "$2") || return;
+	elif [[ "$1" == "host"]]; then
+		HOST="$2";
 	fi
-	listening_
+	ping -O $HOST $ifc | while read -r line; do #ping and read lines
+		echo -e "${HOST}: ";
+		if [[ "$line" =~ "Unreachable" ]] || [[ "$line" =~ "no answer" ]]; then
+		#no answer ---> eroor mode ---> alert & log
+			[ "$errormode" -eq 1 ] && {	
+				echo -e " \e[101;1;97m WARNING: $line\e[m\n";
+				continue;
+			}
+			echo -e "\n\e[101;1;97m${hostdown_msg}\e[m\n" && echo -e "${DATE}: ${hostdown_msg}" >> "$LOG";
+			echo -e "\e[101;1;97m$line\e[m\n" && echo -e "${DATE}: ${line}" >> "$LOG";
+			errormode=1;
+		else #good answer
+			[ "$errormode" -eq 0 ] && {
+				echo -e "$line\n";
+				continue;
+			}
+			let aae=aae+1; #count aae
+			[ "$aae" -eq 1 ] && { #first good ping after error
+				echo -e "\n\e[42;1;97m${DATE}: HOST IS UP\e[m\n" && echo -e "${DATE} HOST IS UP" >> "$LOG";
+				echo -e "\e[42;1;97m${line}\e[m\n" && echo -e "${DATE}: ${line}" >> "$LOG";
+				continue;
+			}
+			[ "$aae" -eq 30 ] && { #30 good ping after error ends error mode
+				echo -e "\n\e[42;1;97m${hoststable_msg}\e[m\n" && echo -e "${DATE}: ${hoststable_msg}" >> "$LOG";
+				echo -e "\e[42;1;97m${line}\e[m\n" && echo -e "${DATE}: ${line}" >> "$LOG";
+				aae=0; #reset AAE
+				errormode=0;
+				continue;
+			}
+			echo -e "\e[42;1;97m${line}\e[m\n";
+		fi
+	done &
+	pingpids=(${pingpids[@]} $!)
 }
-function view-log() {
+function viewLog() {
 	echo "
-	what text editor do you want to use?
+what text editor do you want to use?
 
-	1) Nano/Pico (Default)
-	2) Vi
-	3) Vim
-	4) gedit
-	5) emacs
-
+1) nano (Default)
+3) Vim
+5) emacs
+4) gedit
+*) nano
 	"
 	read -rn1 editor
 	case $editor in
@@ -129,7 +90,7 @@ function view-log() {
 			nano "$LOG";;
 	esac
 }
-function listening_() { #exit?
+function keyEventListener() {
 	while true; do
 		read -rsn1 option;
 		if [[ "$option" == "" ]] || [[ "$option" == "q" ]]; then
@@ -138,7 +99,7 @@ function listening_() { #exit?
 		elif [[ "$option" == "l" ]]; then
 			tmux new-session -d -s pmon
 			tmux send-keys "source pmon_functions.sh" C-m
-			tmux send-keys "view-log"
+			tmux send-keys "vieLog"
 			tmux attach-session -t pmon
 		fi
 	done
