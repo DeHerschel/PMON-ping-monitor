0c4bdfd6ca44d8b54ed3ec90457e45ff32de6857
DeHerschel <e.deherschel@gmail.com>
2021-05-04 02:26:14 +0200

    changes

--- GUI/routes/web.php
+++ GUI/routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
+    return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
